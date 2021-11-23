5d227ed54dc9e222f58ee96c16c9abd74334562d
Ixicutor <ghislain.ast@gmail.com>
2021-11-23 17:28:31 +0200

    App

---
+++ spring-s3-react-file-upload/src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,27 @@
+package com.amigoscode.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+          "AKIAVPKZ4EFDVC2N6K6T",
+          "RCB8a/Sr7+hjipnd6RU7eZlp6OF+CtVFxeOpyywS"
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion("eu-north-1")
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+}
